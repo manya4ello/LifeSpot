7cc71f0581110748a030ba1e9d9bd13a925189f1
Oleg Freidkin <oleg.freidkin@gmail.com>
2022-06-13 08:01:12 +0300

    Залил на GitHub

--- LifeSpot/Program.cs
+++ LifeSpot/Program.cs
@@ -6,12 +6,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+ 
 namespace LifeSpot
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) 
         {
             CreateHostBuilder(args).Build().Run();
         }
