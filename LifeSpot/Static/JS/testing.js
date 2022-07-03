e00331643ac8542551dc94d22eea2bb098bed24a
Oleg Freidkin <oleg.freidkin@gmail.com>
2022-07-03 15:49:50 +0300

    30.3.2

--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -9,3 +9,14 @@ alert(`Количество элементов на странице:  ${element
     let userName = prompt("Пожалуйста, введите ваше имя");
     alert(`Приветствуем, ${userName}. В вашем имени ${userName.length} символов`)
 
+const saveInput = function () {
+    // Вытащим значение текстового поля (как у нас уже делается при фильтрации)
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+
+    // Покажем окно с прошлым и новым значением
+    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+        + '\n' + 'Текущий ввод: ' + currentInput);
+
+    // Сохраним новое значение в контекст
+    this.lastInput = currentInput;
+}
